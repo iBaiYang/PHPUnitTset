257962340cb980017fa0d82ef7e81cccd65a9466
dingdong <dingdong@test.com>
2023-03-23 14:04:27 +0800

    FlyweightTest add

---
+++ DesignPatterns/Structural/Flyweight/Text.php
@@ -0,0 +1,10 @@
+<?php
+namespace DesignPatterns\Structural\Flyweight;
+
+/**
+ * This is the interface that all flyweights need to implement
+ */
+interface Text
+{
+    public function render(string $extrinsicState): string;
+}
